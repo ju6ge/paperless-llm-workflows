5cb899b19d34439a15059d006848384aab507d5f
ju6ge <judge@felixrichter.tech>
2026-05-22 00:06:46 +0200

    truncate custom_field string fields to 128 chars
    the alternative would be to throw an error. But with that the field will
    remain empty. This could trigger a loop on request completion. If
    paperless is configured to send a webhook when an document is altered
    and an empty custom_field is present. This could be revisited if
    an error feed back is implement in form of an suggested error tag.
    But that would also make the entire webhook pipeline even more
    complicated to manage, since even more state is involved … At present
    it is already a good idea to setup webhook not to fire while the
    `processing_tag` is present!

--- src/types.rs
+++ src/types.rs
@@ -82,9 +82,12 @@ impl FieldExtract {
                 })
             }
             paperless_api_client::types::DataTypeEnum::String => {
-                let _parsed_value: String = serde_json::from_value(self.value.clone())?;
+                let mut parsed_value: String = serde_json::from_value(self.value.clone())?;
+                // string custom fields in paperless may only be at most 128 chars
+                parsed_value.truncate(127);
+                parsed_value.push('…');
                 Ok(CustomFieldInstance {
-                    value: Some(self.value.clone()),
+                    value: Some(Value::from(parsed_value)),
                     field: custom_field_spec.id,
                 })
             }
